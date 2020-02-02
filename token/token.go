9a78e21df5b80dfe8326f73086d8b078aab45556
Kushiro Taichi <k.taichi0315@gmail.com>
2020-02-02 21:50:03 +0900

    add token.go

---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type TokenType string
+
+type Token struct {
+  Type    TokenType
+  Literal string
+}
+
+const (
+  ILLEGAL = "ILLEGAL"
+  EOF     = "EOF"
+
+  // identifiers + literals
+  IDENT = "IDENT"
+  INT   = "INT"
+
+  // Operators
+  ASSIGN = "="
+  PLUS   = "+"
+
+  // Delemiters
+  COMMA     = ","
+  SEMICOLON = ";"
+
+  LPAREN = "("
+  RPAREN = ")"
+  LBRACE = "{"
+  RBRACE = "}"
+
+  // Keywords
+  FUNCTION = "FUNCTION"
+  LET      = "LET"
+)
+
