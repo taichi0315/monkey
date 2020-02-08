9ec8230ef1f17f1d82ba5fd2fe195490122f3632
Kushiro Taichi <k.taichi0315@gmail.com>
2020-02-09 01:38:50 +0900

    add LetStatement

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+  "github.com/taichi0315/monkey/token"
+)
+
 type Node interface {
   TokenLiteral() string
 }
@@ -25,3 +29,20 @@ func (p *Program) TokenLiteral() string {
     return ""
   }
 }
+
+type LetStatement struct {
+  Token token.Token
+  Name  *Indentifier
+  Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+  Token token.Token
+  Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
