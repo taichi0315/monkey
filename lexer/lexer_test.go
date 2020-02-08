6ae67cd0e10e449a8aef527d813b87fe39d9af77
Kushiro Taichi <k.taichi0315@gmail.com>
2020-02-08 17:59:35 +0900

    fixed lexer for ==,!=

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -95,6 +95,14 @@ if (5 < 10) {
     {token.RETURN, "return"},
     {token.FALSE, "false"},
     {token.RBRACE, "}"},
+    {token.INT, "10"},
+    {token.EQ, "=="},
+    {token.INT, "10"},
+    {token.SEMICOLON, ";"},
+    {token.INT, "10"},
+    {token.NOT_EQ, "!="},
+    {token.INT, "9"},
+    {token.SEMICOLON, ";"},
     {token.EOF, ""},
   }
 
